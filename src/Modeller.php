f2c91eb2f39adf4a7a1e360e7e8c7f32450e6592
Fabricio Jakubec <fabricio.jakubec@fpt.sk>
2021-12-01 12:14:50 +0100

    added nested models support

--- src/Modeller.php
+++ src/Modeller.php
@@ -9,8 +9,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Fabricio872\ApiModeller\Annotations\Resource;
 use Fabricio872\ApiModeller\Annotations\ResourceInterface;
 use Fabricio872\ApiModeller\Annotations\Resources;
+use Fabricio872\ApiModeller\Annotations\SubModel;
 use Fabricio872\ApiModeller\ClientAdapter\ClientInterface;
-use GuzzleHttp\Exception\ClientException;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -41,6 +41,11 @@ class Modeller
      */
     private $repo;
 
+    /**
+     * @param Reader $reader
+     * @param ClientInterface $client
+     * @param Environment $twig
+     */
     public function __construct(Reader $reader, ClientInterface $client, Environment $twig)
     {
         $this->reader = $reader;
@@ -54,16 +59,14 @@ class Modeller
     public function getData()
     {
         $normalizedContent = self::getSerializer()->decode((string)$this->getRawData(), $this->getAnnotation()->type);
-        $return = new ArrayCollection();
-        if (array_values($normalizedContent) === $normalizedContent) {
-            foreach ($normalizedContent as $normalizedItem) {
-                $return->add(self::getSerializer()->denormalize($normalizedItem, $this->repo->getModel()));
-            }
-            return $return;
-        }
-        return self::getSerializer()->denormalize($normalizedContent, $this->repo->getModel());
+
+        return $this->modelBuilder($normalizedContent, $this->repo->getModel());
     }
 
+    /**
+     * @return string
+     * @throws RuntimeError
+     */
     public function getRawData()
     {
         return $this->client->request(
@@ -83,26 +86,90 @@ class Modeller
         return $this;
     }
 
+    /**
+     * @return string
+     * @throws \Exception
+     */
     public function getMethod()
     {
         return $this->getAnnotation()->method;
     }
 
+    /**
+     * @return string
+     * @throws RuntimeError
+     */
     public function getEndpoint()
     {
         return self::renderEndpoint($this->getAnnotation(), $this->repo);
     }
 
+    /**
+     * @return array
+     * @throws \Exception
+     */
     public function getOptions()
     {
         return array_merge_recursive($this->getAnnotation()->options, $this->repo->getOptions());
     }
 
+    /**
+     * @return Resource
+     * @throws \Exception
+     */
     private function getAnnotation()
     {
         return $this->getResource($this->repo->getModel(), $this->repo->getIdentifier());
     }
 
+    /**
+     * @param array $normalizedData
+     * @param string $model
+     * @return array|ArrayCollection|object
+     * @throws \ReflectionException
+     */
+    private function modelBuilder(array $normalizedData, string $model)
+    {
+        if (array_values($normalizedData) === $normalizedData) {
+            $return = new ArrayCollection();
+            foreach ($normalizedData as $normalizedItem) {
+
+                $return->add($this->subModelBuilder(self::getSerializer()->denormalize($normalizedItem, $model)));
+            }
+            return $return;
+        }
+        return $this->subModelBuilder(self::getSerializer()->denormalize($normalizedData, $model));
+    }
+
+    /**
+     * @param array|object $denormalized
+     * @return array|object
+     * @throws \ReflectionException
+     */
+    private function subModelBuilder($denormalized)
+    {
+        $reflectionClass = new \ReflectionClass($denormalized);
+
+        foreach ($reflectionClass->getProperties() as $reflectionProperty) {
+
+            $subModel = $this->reader->getPropertyAnnotation($reflectionProperty, SubModel::class);
+            if ($subModel instanceof SubModel) {
+                $reflectionProperty->setAccessible(true);
+                $reflectionProperty->setValue(
+                    $denormalized,
+                    $this->modelBuilder(
+                        $reflectionProperty->getValue($denormalized),
+                        $subModel->model
+                    )
+                );
+            }
+        }
+        return $denormalized;
+    }
+
+    /**
+     * @return Serializer
+     */
     private static function getSerializer()
     {
         $encoders = [new XmlEncoder(), new JsonEncoder()];
@@ -111,6 +178,14 @@ class Modeller
         return new Serializer($normalizers, $encoders);
     }
 
+    /**
+     * @param Resource $annotation
+     * @param Repo $repo
+     * @return string
+     * @throws RuntimeError
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\SyntaxError
+     */
     private function renderEndpoint(Resource $annotation, Repo $repo): string
     {
         try {
@@ -123,13 +198,19 @@ class Modeller
         return $rendered;
     }
 
+    /**
+     * @param string $model
+     * @param string $identifier
+     * @return Resource
+     * @throws \ReflectionException
+     */
     private function getResource(string $model, string $identifier): Resource
     {
         $reflection = new \ReflectionClass($model);
 
         $resourceInterface = $this->reader->getClassAnnotation($reflection, ResourceInterface::class);
         if ($resourceInterface instanceof Resources) {
-            if (! array_key_exists($identifier, $resourceInterface->resources)) {
+            if (!array_key_exists($identifier, $resourceInterface->resources)) {
                 throw new \Exception(sprintf('Identifier: "%s" does not exists in Model: "%s"', $identifier, $model));
             }
             return $resourceInterface->resources[$identifier];
